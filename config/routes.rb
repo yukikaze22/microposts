409957e0761281372ca4472048f1f336c181f480
taro.kirameki <yukikaze2234@yahoo.co.jp>
2020-06-09 05:11:23 +0000

    usee login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: "toppages#index"
+  
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  
+  get "signup", to: "users#new"
+  resources :users, only: [:index, :show, :new, :create]
 end
