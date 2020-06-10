1ec9e5a531b69a8202d1bf6bfa3176e50a573809
taro.kirameki <yukikaze2234@yahoo.co.jp>
2020-06-10 03:58:08 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get 'microposts/create'
+  get 'microposts/destroy'
   root to: "toppages#index"
   
   get "login", to: "sessions#new"
@@ -10,4 +9,6 @@ Rails.application.routes.draw do
   
   get "signup", to: "users#new"
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :microposts, only: [:create, :destroy]
 end
